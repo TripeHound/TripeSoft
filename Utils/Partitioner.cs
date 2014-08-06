659015b06791e568afca9eb76f9c67d95de31dc2
TripeHound <TripeHound@aldurslair.com>
2014-08-06 15:22:03 +0100

    Adding Partitioner class and playing with test-framework!

---
+++ Utils/Partitioner.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TripeSoft.Utils
+{
+	public class Partitioner
+	{
+		public class Partition
+		{
+			public enum PartType
+			{
+				Unassigned,
+				FixedSize,
+				Truncatable
+			}
+			public long Offset { get; private set; }
+			public long Length { get; private set; }
+			public PartType Type { get; private set; }
+
+			public Partition( long offset, long length, PartType type )
+			{
+				Offset = offset;
+				Length = length;
+				Type   = type;
+			}
+		}
+
+		private List<Partition> partitions;
+
+		public Partitioner( long maxLength )
+		{
+			partitions = new List<Partition>();
+			partitions.Add( new Partition( 0, maxLength, Partition.PartType.Unassigned ) );
+		}
+
+		public Partition ContainingPartition( long offset )
+		{
+			foreach( Partition partition in partitions )
+			{
+				if( offset >= partition.Offset && offset < partition.Offset + partition.Length )
+					return partition;
+			}
+			return null;
+		}
+	}
+}
