c25354040c17f8d831644a1b6b1a20452304203b
TripeHound <TripeHound@aldurslair.com>
2014-08-07 17:29:18 +0100

    Playing with test; refactoring CreatePartition().

--- Tests/PartitionerTests.cs
+++ Tests/PartitionerTests.cs
@@ -11,9 +11,13 @@ namespace Tests
 	/// Summary description for UnitTest1
 	/// </summary>
 	[TestClass]
-	public class PartitionerTests
+	public class PartitionTests
 	{
-		public PartitionerTests()
+		Partitioner.Partition unassigned = new Partitioner.Partition( 10, 100, Partitioner.Partition.States.Unassigned );
+		Partitioner.Partition  tentative = new Partitioner.Partition( 10, 100, Partitioner.Partition.States.Assigned);//WRONG!
+		Partitioner.Partition   assigned = new Partitioner.Partition( 10, 100, Partitioner.Partition.States.Assigned );
+
+		public PartitionTests()
 		{
 			//
 			// TODO: Add constructor logic here
@@ -60,15 +64,78 @@ namespace Tests
 		//
 		#endregion
 
+		private void ShouldNotContain( Partitioner partitioner, long offset )
+		{
+			try
+			{
+				Partitioner.Partition partition = partitioner.ContainingPartition( offset );
+				Assert.Fail( "Offset {0} found in {1}", new object[] { offset, partition } );
+			}
+			catch( ArgumentOutOfRangeException ex )
+			{
+				StringAssert.Contains( ex.Message, Partitioner.ExMsg.NotInParentPartition );
+			}
+		}
+
+		[TestMethod]
+		public void TestPartition_IsXXX()
+		{
+			Assert.IsTrue	( unassigned.IsUnassigned,	"Unassigned partition is not unaassigned"	);
+			Assert.IsTrue	( unassigned.IsTruncatable,	"Unassigned partition is not truncatable"	);
+			Assert.IsFalse	( unassigned.IsTentative,	"Unassigned partition is tentative"			);
+			Assert.IsFalse	( unassigned.IsAssigned,	"Unassigned partition is assigned"			);
+			Assert.IsFalse	( tentative.IsUnassigned,	"Tentative partition is unassigned"			);
+			Assert.IsTrue	( tentative.IsTruncatable,	"Tentative partition is not truncatable"	);
+			Assert.IsTrue	( tentative.IsTentative,	"Tentative partition is not tentative"		);
+			Assert.IsFalse	( tentative.IsAssigned,		"Tentative partition is assigned"			);
+			Assert.IsFalse	( assigned.IsUnassigned,	"Assigned partition is unassigned"			);
+			Assert.IsFalse	( assigned.IsTruncatable,	"Assigned partition is truncatable"			);
+			Assert.IsFalse	( assigned.IsTentative,		"Assigned partition is tentative"			);
+			Assert.IsTrue	( assigned.IsAssigned,		"Assigned partition is not assigned"		);
+		}
+
+		[TestMethod]
+		public void TestPartition_MarkAssigned()
+		{
+			try
+			{
+				unassigned.MarkAssigned();
+			}
+			catch( Exception ex )
+			{
+				Assert.Fail( "Unassigned partition could not be marked assigned: " + ex.Message );
+			}
+			try
+			{
+				tentative.MarkAssigned();
+			}
+			catch( Exception ex )
+			{
+				Assert.Fail( "Tentative partition could not be marked assigned: " + ex.Message );
+			}
+			try
+			{
+				assigned.MarkAssigned();
+				Assert.Fail( "Assigned partiton could be marked assigned" );
+			}
+			catch( InvalidOperationException ex )
+			{
+				StringAssert.Contains( Partitioner.ExMsg.PartitionAlreadyAssigned, ex.Message );
+			}
+		}
+
 		[TestMethod]
 		public void TestContain()
 		{
 			const long Size = 100 ;
 			Partitioner partitioner = new Partitioner( Size );
-			Assert.AreEqual		( null, partitioner.ContainingPartition(     -1 ), "Negative offset is contained" );
+			ShouldNotContain( partitioner, -1 );
+			ShouldNotContain( partitioner, Size );
+
 			Assert.AreNotEqual	( null, partitioner.ContainingPartition(      0 ), "Beginging is not contained" );
 			Assert.AreNotEqual	( null, partitioner.ContainingPartition( Size-1 ), "End is not contained" );
-			Assert.AreEqual		( null, partitioner.ContainingPartition( Size   ), "Overlarge offset is contained" );
+
+			System.Diagnostics.Debug.WriteLine( partitioner.ToString() );
 		}
 	}
 }
--- Utils/Partitioner.cs
+++ Utils/Partitioner.cs
@@ -9,50 +9,185 @@ namespace TripeSoft.Utils
 	{
 		public class Partition
 		{
-			public enum PartType
-			{
-				Unassigned,
-				FixedSize,
-				Truncatable
-			}
-			public long Offset { get; private set; }
-			public long Length { get; private set; }
-			public PartType Type { get; private set; }
+			public enum		States	{ Unassigned, Tentative, Assigned } ;
+			public long		Offset	{ get; private set; }
+			public long		Length	{ get; private set; }
+			public States	State	{ get; private set; }
 
-			public Partition( long offset, long length, PartType type )
+			//TODO: Members/properties to hold external type and/or object?
+
+			/// <summary>
+			/// Create a Partition class of the given size and type.
+			/// </summary>
+			/// <param name="offset">Offset (within containing space) of the new partition.</param>
+			/// <param name="length">Length of the new partition.</param>
+			public Partition( long offset, long length, States state )
 			{
-				Offset = offset;
-				Length = length;
-				Type   = type;
+				Offset	= offset;
+				Length	= length;
+				State	= state;
 			}
+
+			public override string ToString()
+			{
+				return String.Format( "[{0},{1},{2}]", Offset, Length, State );
+			}
+
+			public bool IsUnassigned
+			{
+				get { return State == States.Unassigned; }
+			}
+			public bool IsTruncatable
+			{
+				get { return State != States.Assigned; }
+			}
+			public bool IsTentative
+			{
+				get { return State == States.Tentative; }
+			}
+			public bool IsAssigned
+			{
+				get { return State == States.Assigned; }
+			}
+
+			/// <summary>
+			/// Marks (an unassigned) partition as in use.
+			/// </summary>
+			/// <exception cref="InvalidOperationException">The partition space has already been assigned.</exception>
+			public void MarkAssigned()
+			{
+				if( IsAssigned )
+					throw new InvalidOperationException( ExMsg.PartitionAlreadyAssigned );
+				State = States.Assigned;
+			}
+
+			/// <summary>
+			/// 
+			/// </summary>
+			/// <param name="offset"></param>
+			/// <returns></returns>
+			public Partition TruncateBefore( long offset )
+			{
+				Partition remainder = null;
+				if( !IsTruncatable )
+					throw new InvalidOperationException( "Partition is not truncatable" );
+				if( offset < Offset || offset > NextOffset )
+					throw new ArgumentOutOfRangeException( "offset", offset, "Truncation point is outside partiton" );
+				if( offset == NextOffset )
+					throw new Exception( "TODO: Truncate at beginning" );
+				if( offset < NextOffset )
+					remainder = new Partition( offset, Remainder( offset ), States.Unassigned );
+				Length = offset - Offset;
+				if( State == States.Tentative )
+					State = States.Assigned;
+				return remainder;
+			}
+
+			/// <summary>
+			/// The offset of the location just beyond the end of the parition.
+			/// </summary>
 			public long NextOffset
 			{
 				get {
 					return Offset + Length;
 				}
 			}
-			public long Remainder( 
+
+			/// <summary>
+			/// Determines the length of the remainder of the current partition beyond a given offset.
+			/// </summary>
+			/// <param name="offset">Offset (from parent).</param>
+			/// <returns>The length remaining.</returns>
+			/// <exception cref="ArgumentOutOfRangeException">The given offset does not lie within the partition.</exception>
+			public long Remainder( long offset )
 			{
-				get {
-					return 
-				}
+				if( !Contains( offset ) )
+					throw new ArgumentOutOfRangeException( "offset", offset, "Offset not within partition" ) ;
+				return NextOffset - offset;
+			}
+
+			/// <summary>
+			/// Determines if a given offset is included in the partition.
+			/// </summary>
+			/// <param name="offset">Offset to test.</param>
+			/// <returns>True if the offset is contained in the partition.</returns>
+			public bool Contains( long offset )
+			{
+				return offset >= Offset && offset < NextOffset;
+			}
+
+			/// <summary>
+			/// Determines if the area specified by the offset and length lies wholly within the current partition.
+			/// </summary>
+			/// <param name="offset">Offset (from parent)</param>
+			/// <param name="length">Length</param>
+			/// <returns>'true' if the specified region falls entirely within the current partition.</returns>
+			public bool Contains( long offset, long length )
+			{
+				return Contains( offset ) && ( offset + length ) <= NextOffset;
+			}
+			/// <summary>
+			/// Determines if the given partition lies wholly within the current partition.
+			/// </summary>
+			/// <param name="partition">Partition to check.</param>
+			/// <returns>'true' if the specified region falls entirely within the current partition.</returns>
+			public bool Contains( Partition partition )
+			{
+				return Contains( partition.Offset, partition.Length );
+			}
+
+			/// <summary>
+			/// Determines if the area specified by the offset and length exactly matches the current partition.
+			/// </summary>
+			/// <param name="offset">Offset (from parent)</param>
+			/// <param name="length">Length</param>
+			/// <returns>'true' if the specified region exactly matches the current partition.</returns>
+			public bool Matches( long offset, long length )
+			{
+				return offset == Offset
+					&& length == Length;
+			}
+			/// <summary>
+			/// Determines if the given partition exactly matches the current partition (ignoring type).
+			/// </summary>
+			/// <param name="offset">Offset (from parent)</param>
+			/// <param name="length">Length</param>
+			/// <returns>'true' if the specified region exactly matches the current partition.</returns>
+			public bool Matches( Partition partition )
+			{
+				return Matches( partition.Offset, partition.Length );
 			}
 		}
 
 		private List<Partition> partitions;
 		public long Length { get; private set; }
 
+		public static class ExMsg
+		{
+			public const string NotInParentPartition		= "Outside partitioned space";
+			public const string PartitionAlreadyAssigned	= "Partition has already been assigned";
+		}
 		public Partitioner( long length )
 		{
 			Length = length;
 			partitions = new List<Partition>();
-			partitions.Add( new Partition( 0, Length, Partition.PartType.Unassigned ) );
+			partitions.Add( new Partition( 0, Length, Partition.States.Unassigned ) );
 		}
 
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendFormat( "Length:{0} Partitions:{1}\n", Length, partitions.Count );
+			foreach( Partition partition in partitions )
+			{
+				sb.Append( partition.ToString() + "\n" );
+			}
+			return sb.ToString();
+		}
 		public Partition ContainingPartition( long offset )
 		{
 			if( offset < 0 || offset >= Length )
-				throw new ArgumentOutOfRangeException( "offset", offset, "Outside partitioned space" );
+				throw new ArgumentOutOfRangeException( "offset", offset, ExMsg.NotInParentPartition );
 			foreach( Partition partition in partitions )
 			{
 				if( offset >= partition.Offset && offset < partition.Offset + partition.Length )
@@ -61,19 +196,63 @@ namespace TripeSoft.Utils
 			throw new ArgumentOutOfRangeException( "offset", offset, "Not found within any partition" );
 		}
 
+		/// <summary>
+		/// Create a new partition of size 'length' starting at 'offset'.  The area must currently be unassigned.
+		/// </summary>
+		/// <param name="offset">Offset of the beginning of the partition.</param>
+		/// <param name="length">Length of the partition.</param>
+		/// <returns>The new Partition</returns>
+		/// <exception cref="ArgumentOutOfRangeException"
 		public Partition CreatePartition( long offset, long length )
 		{
-			Partition existing = ContainingPartition( offset );
-			Partition newPart  = new Partition( offset, length, Partition.PartType.FixedSize ) ;
-			if( existing.Type == Partition.PartType.FixedSize )
+			Partition existing  = ContainingPartition( offset );
+			Partition partition = null;
+			Partition remainder = null;
+
+			//Check:
+			//	The start of the new partition is in unassigned space, and that it does not cross into another
+			//	partition (it is assumed that there will not be adjacent blocks of unassigned space).  If the
+			//	new partition leaves unassigned space after it, create another partition to represent it.
+			//
+
+
+			//	Check that the start of the new partition lies in an unassigned partition and that the new partition
+			//	does not extend into the next one (it is assumed that there will NOT be adjacent unassigned partitions).
+			//
+			if( !existing.IsUnassigned )
 				throw new ArgumentOutOfRangeException( "offset", offset, "Already assigned to a partition" );
-			if( newPart.NextOffset > existing.NextOffset )
+			if( !existing.Contains( offset, length ) )
 				throw new ArgumentOutOfRangeException( "length", length, "Crosses a partition boundary" );
-			if( newPart.NextOffset < existing.NextOffset )
-				partitions.Add( new Partition( newPart.NextOffset, existing.Remainder( newPart.NextOffset ), existing.Type ) );
-			existing.Length = offset - existing.Offset;
-			//TODO:
-			return existing;
+
+			//	If the new partition EXACTLY matches the unassigned one, change its type and return.
+			//
+			if( existing.Matches( offset, length ) )
+			{
+				existing.MarkAssigned();
+				return existing ;
+			}
+
+			//	If the new partition is at the START of the unassigned space, change the type, create a
+			//	partition for the remainder, truncate the existing partition and add the remainder to the list.
+			//
+			if( existing.Offset == offset )
+			{
+				remainder = existing.TruncateBefore( offset + length );
+				existing.MarkAssigned();
+				partitions.Add( remainder ) ;
+				return existing ;
+			}
+
+			//	Otherwise, the new partition DIVIDES the existing unassigned space: create partitions for the
+			//	new space (and the remainder, if any).  Truncate the existing partition and the new/remainder.
+			//
+			partition = existing.TruncateBefore( offset );
+			if( partition.Length > length )
+				remainder = partition.TruncateBefore( offset + length );
+			partitions.Add( partition );
+			if( remainder != null )
+				partitions.Add( remainder );
+			return partition;
 		}
 	}
 }
